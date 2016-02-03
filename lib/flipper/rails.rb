71a45aae5f05b649e79552a934ccab08d0d1260e
Brendan Mulholland <me@bmulholland.ca>
2016-02-03 12:59:45 -0800

    Include adapters so we don't have to do so manually

--- lib/flipper/rails.rb
+++ lib/flipper/rails.rb
@@ -12,6 +12,14 @@ end
 
 require 'flipper'
 
+require 'flipper/adapters/active_record'
+require 'flipper/adapters/instrumented'
+require 'flipper/adapters/memoizable'
+require 'flipper/adapters/memory'
+require 'flipper/adapters/mongo'
+require 'flipper/adapters/pstore'
+require 'flipper/adapters/redis'
+
 require 'flipper/rails/version'
 require 'flipper/rails/options'
 require 'flipper/rails/railtie'
